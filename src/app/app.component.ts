24aeb96800f7fa2064c81ca317623c8990c95687
eramit95 <eramit95@gmail.com>
2019-05-05 13:46:50 +0530

    adding audio player component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Track } from 'ngx-audio-player';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'audio-player';
+  msaapDisplayTitle = true;
+  msaapDisplayPlayList = true;
+  msaapPageSizeOptions = [2, 4, 6];
+
+  // Material Style Advance Audio Player Playlist
+  msaapPlaylist: Track[] = [
+      {
+          title: 'Audio One Title',
+          link: 'https://www.dropbox.com/s/2t968nilfzbxflv/Mechanical%20Sundariye.mp3?dl=1'
+      },
+      {
+          title: 'Audio Two Title',
+          link: 'https://song.link/album/i/1460451396'
+      },
+      {
+          title: 'Audio Three Title',
+          link: 'https://song.link/album/i/1460451396'
+      }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import library module
+import { NgxAudioPlayerModule } from 'ngx-audio-player';
+import { AudioPlayerComponent } from './audio-player/audio-player.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AudioPlayerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxAudioPlayerModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
